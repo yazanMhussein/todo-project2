5348e42ea79eb3f2ffb780345bc93387ad4f7df7
yazanmike <yazan.mike@gmail.com>
2024-03-23 12:26:22 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*
-If the input was empty consider the answer to be an "invalid" string.
-
-Consider putting all the answers into an array.
-
-Traverse the array and print the items on the console.
-
-Confirm that your code is following the single responsibility rule. Each function should only do one thing.
-
-All functions and variables should have a meaningful name (use camelCase).
-
-*/ 
 /* 
 we have to ask three question to the user and they have to be yes or no question
 if the user enter a nothing then give him a invalid string 
@@ -23,6 +11,7 @@ let questionList = function(){
    answers.push(question2 || "invalid");
    let question3 =prompt("to you like to playing video games (yes/no)")
    answers.push(question3 || "invalid");
+   
    console.log("your answer is:")
    console.log(answers)
 
